8124e44875d11fe214ae848a8d2a681b07842347
takuya-kawano <118519402+takuya-kawano@users.noreply.github.com>
2024-02-07 16:55:35 +0900

    Update tsup-shim.js
    20240207: kawano: region変更

--- tsup-shim.js
+++ tsup-shim.js
@@ -1,4 +1,5 @@
 export const useRuntimeConfig = () => ({
-  region: process.env.NUXT_REGION ?? "ap-northeast-1",
+  //region: process.env.NUXT_REGION ?? "ap-northeast-1",
+  region: process.env.NUXT_REGION ?? "eu-west-1",
   bucketName: process.env.NUXT_BUCKET_NAME ?? "",
 });
